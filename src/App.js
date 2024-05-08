fa5c86c6adf1f1f1f281ec16cff6b37197368b49
Abhay Singh <78447760+Abhaysingh77@users.noreply.github.com>
2024-05-08 15:54:55 +0530

    added x-stopwatch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useRef } from "react";
 
 function App() {
+  const [timeInSec, setTimeInSec] = useState("00");
+  const [timeInMin, setTimeInMin] = useState(0);
+  const [isOn, setIsOn] = useState(false);
+  const time_intrvl = useRef(null);
+
+  const handleReset = () => {
+    clearInterval(time_intrvl.current);
+    setTimeInSec("00");
+    setTimeInMin(0)
+    setIsOn(false);
+  };
+
+  const handleStart = () => {
+    setIsOn(true);
+    time_intrvl.current = setInterval(() => {
+      setTimeInSec((prev) => {
+        if (Number(prev) === 59) {
+          setTimeInMin(timeInMin+1);
+          return "00";
+        } else {
+          return prev < 9 ? "0" + (Number(prev) + 1) : String(Number(prev) + 1);
+        }
+      });
+    }, 1000);
+  };
+  const handleStop = () => {
+    clearInterval(time_intrvl.current);
+    setIsOn(false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Stopwatch</h1>
+      <p>
+        Time: {timeInMin}:{timeInSec}
+      </p>
+      {!isOn ? (
+        <button onClick={handleStart}>Start</button>
+      ) : (
+        <button onClick={handleStop}>Stop</button>
+      )}
+      <button onClick={handleReset}>Reset</button>
+    </>
   );
 }
 
